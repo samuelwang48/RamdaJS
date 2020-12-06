ddb731686ba48b34b52db197e4b09be117fe9b85
Samuel Wang <sam.ajax@gmail.com>
2020-12-06 03:49:41 -0500

    Convert BinaryConvert to ramda

--- Maths/BinaryConvert.js
+++ Maths/BinaryConvert.js
@@ -1,10 +1,33 @@
-const BinaryConvert = (number) => {
-  const result = []
-  let i
-  for (i = number; i > 0; i = parseInt(i / 2)) {
-    result.push(i % 2) // push the value (remainder)to array
-  } return Number(result.reverse().join(''))
-  // reverse index of array as string ,join and change the type of value to become Number
-}
-// call function and value as parameter to passing the value
+import * as R from 'ramda';
+
+/**
+ * Author: Samuel Wang
+ * Original Author: https://github.com/arwysyah
+ * License: GPL-3.0 or later
+ */
+
+const BinaryConvert = (n) => R.pipe(
+  R.converge(
+    (a, b) => R.times(
+      (x) => parseInt( R.divide(b, 2 ** x ))  % 2, a
+    ),
+    [
+      R.pipe(
+        Math.log2,
+        parseInt,
+        R.inc
+      ),
+      R.identity
+    ]
+  ),
+  R.reverse,
+  R.join(''),
+  Number
+)(n);
+
+
+//console.log(BinaryConvert(8))
+//console.log(BinaryConvert(12))
+//console.log(BinaryConvert(817621))
+
 export { BinaryConvert }
--- Maths/test/BInaryConvert.test.js
+++ Maths/test/BInaryConvert.test.js
@@ -1,8 +1,11 @@
 import { BinaryConvert } from '../BinaryConvert'
 
 describe('Binary Convert', () => {
-  it('should return the correct value', () => {
+  it('should return the correct value #1', () => {
     expect(BinaryConvert(12)).toBe(1100)
+  })
+  it('should return the correct value #2', () => {
+    expect(BinaryConvert(817621)).toBe(11000111100111010101)
   })
   it('should return the correct value of the sum from two number', () => {
     expect(BinaryConvert(12 + 2)).toBe(1110)
