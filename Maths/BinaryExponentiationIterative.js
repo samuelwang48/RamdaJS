478802508dbd1b3fb58bdee7945aa2a38f4ec6cd
Samuel Wang <sam.ajax@gmail.com>
2020-12-06 23:39:08 -0500

    unfinished work BinaryExponentiationIterative.js

--- Maths/BinaryExponentiationIterative.js
+++ Maths/BinaryExponentiationIterative.js
@@ -1,3 +1,5 @@
+import * as R from 'ramda';
+
 // To calculate x^n i.e. exponent(x, n) in O(log n) time in iterative way
 // n is an integer and n >= 0
 
@@ -11,14 +13,45 @@
 // exponent(x, n)
 //          = exponent(x*x, floor(n/2))   ; if n is odd
 //          = x*exponent(x*x, floor(n/2)) ; if n is even
+
 const exponent = (x, n) => {
   let ans = 1
   while (n > 0) {
+    console.log(':', n);
     if (n % 2 !== 0) ans *= x
     n = Math.floor(n / 2)
     if (n > 0) x *= x
   }
   return ans
 }
+
+const exponent_ = (x, n) => {
+  return R.useWith(
+    (x, n, r) => {
+      R.until(
+        R.equals(0),
+        (n) => {
+          return R.pipe(
+            R.when( R.pipe(R.modulo(R.__, 2), R.equals(0), R.not), (n) => {r *= x; return n;} ),
+            (n) => Math.floor(n / 2),
+            R.when( R.gt(R.__, 0), (n) => {x *= x; return n;} )
+          )(n)
+        }
+      )(n);
+      return r;
+    },
+    [
+      R.identity,
+      R.identity,
+      R.defaultTo(1)
+    ]
+  )(x, n, undefined)
+  
+}
+
+
+console.log(exponent_.call(null, 2, 3))
+console.log('----------')
+console.log(exponent.call(null, 2, 3))
 
 export { exponent }
